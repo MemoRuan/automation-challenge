47bbdaa6c6b3056f257c70bf9b19bf442e5a024c
MemoRuan <memoruan_27@hotmail.com>
2024-04-18 22:00:29 -0600

    Update TestRunner.java

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -1,2 +1,11 @@
-package runner;public class TestRunner {
+package runner;
+
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+//Test runner enables the execution via cmd command
+@CucumberOptions(features = {"src/test/java/features"}, glue = "steps")
+public class TestRunner extends AbstractTestNGCucumberTests {
+
 }
